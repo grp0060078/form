6cb4a0858d6c33ff265ce985af8d035d4eace2f9
grp0060078 <142966658+grp0060078@users.noreply.github.com>
2023-09-22 14:05:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+    function addData() {
+            const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const address = document.getElementById('address').value;
+    const pincode = document.getElementById('pincode').value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const food = document.getElementById('food').value;
+    const state = document.getElementById('state').value;
+    const country = document.getElementById('country').value;
+
+    const table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow(table.rows.length);
+
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+    const cell6 = newRow.insertCell(5);
+    const cell7 = newRow.insertCell(6);
+    const cell8 = newRow.insertCell(7);
+
+    cell1.innerHTML = firstName;
+    cell2.innerHTML = lastName;
+    cell3.innerHTML = address;
+    cell4.innerHTML = pincode;
+    cell5.innerHTML = gender;
+    cell6.innerHTML = food;
+    cell7.innerHTML = state;
+    cell8.innerHTML = country;
+
+    
+    document.getElementById('dataForm').reset();
+}
